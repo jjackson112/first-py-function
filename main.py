ace24222f17c47c4e483f18c0c770797f31f101b
jjackson112 <jjackson11211@gmail.com>
2025-04-02 17:26:51 -0400

    Updated function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+
 def calculate_pay(hours_worked, pay_per_hour):
-  return hours_worked * pay_per_hour
+    if hours_worked > 40: 
+        overtime_hours = hours_worked - 40
+        regular_pay = 40 * pay_per_hour
+        overtime_pay = overtime_hours * pay_per_hour *2
+    return regular_pay + overtime_pay
+    return hours_worked * pay_per_hour
+
 # Worked 40 hours at $20 an hour
 print(calculate_pay(40,20))
 # Worked 50 hours at $20 an hour
